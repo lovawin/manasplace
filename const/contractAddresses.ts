a2045f562c9c168d2758046e509320b0eb2f85b2
lovawin <89751634+lovawin@users.noreply.github.com>
2023-12-21 16:03:17 -0500

    Add files via upload

---
+++ const/contractAddresses.ts
@@ -0,0 +1,10 @@
+
+import { Mumbai, Pulsechain } from "@thirdweb-dev/chains";
+export const NETWORK = Pulsechain;
+
+
+export const MARKETPLACE_ADDRESS = "0x8B03ffdBEA2B692F0034E6490484b3e1eAF1694b";
+
+export const NFT_COLLECTION_ADDRESS = "0x2F9324d6a1Db00C319414aAC92462fB15c570279"
+export const ETHERSCAN_URL = "https://scan.pulsechain.com";
+export const MANA_CONTRACT_ADDRESS = "0xC8e4D0FaB14CCD99f686870B6BE0EA0B95Aa420B"
